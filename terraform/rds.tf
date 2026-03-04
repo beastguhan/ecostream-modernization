49982ed5981d38bf0718db57f209df9323673c38
Guhan M <guhanmg25@gmail.com>
2026-03-04 17:53:30 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,8 +2,10 @@ resource "aws_db_instance" "main" {
   allocated_storage = 20
   engine            = "postgres"
   instance_class    = "db.t3.micro"
-  username          = "admin"
-  password          = "ChangeMe123!"
+  username          = var.db_username
+  password          = var.db_password
   multi_az          = true
   storage_encrypted = true
+
+  skip_final_snapshot = true
 }
