3e36b8d8e8bdb6cab3e2ca6a2a6a85e33710daf2
Guhan M <guhanmg25@gmail.com>
2026-03-04 17:53:16 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,26 @@
 resource "aws_ecs_cluster" "main" {
   name = "ecostream-cluster"
 }
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "ecostream-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = "nginx"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
+}
