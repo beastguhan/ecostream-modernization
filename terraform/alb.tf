a2f1bbcb542181dad0b399620862c1532c0d3486
Guhan M <guhanmg25@gmail.com>
2026-03-04 17:53:02 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -5,7 +5,19 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "app" {
+  name     = "ecostream-target-group"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
 }
